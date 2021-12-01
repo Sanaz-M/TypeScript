1307c09304db3b66fc1d8a9c7b8af784d2e7b3b3
Sanaz M <sanazmohammaddi@gmail.com>
2021-12-01 14:28:33 +0000

    Music Search Engine

---
+++ music/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
